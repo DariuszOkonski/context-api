15ba43003c161151af403868d6258cd79ab0f209
Dariusz Okonski <darek200180@gmail.com>
2021-10-01 00:26:27 +0100

    add double Context to Navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 
-//1,2,3,4,5,6,7,,,,,,,,,,,,,,,21
+//1,2,3,4,5,6,7,8,,,,,,,,,,,,,,21
 function App() {
   return (
     <div className="App">
