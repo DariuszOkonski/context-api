4c160df08d239601c6fe4cf986f0d8660362be55
Dariusz Okonski <darek200180@gmail.com>
2021-09-30 23:31:39 +0100

    initialization

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+
+
+//1,2,,,,,,,,,,,,,,,,,,,,21
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
