10f37c4f2cde7c1ddc4edd3577db3e48a04d1653
Dariusz Okonski <darek200180@gmail.com>
2021-10-01 00:18:19 +0100

    add AuthContext

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 
-//1,2,3,4,5,,,,,,,,,,,,,,,,,21
+//1,2,3,4,5,6,7,,,,,,,,,,,,,,,21
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />  
-        <ThemeToggle />      
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />  
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
